33136158b19ef5049174f61d149e5e2274c00e08
pwsq <xizi@seas.upenn.edu>
2020-09-12 16:53:43 -0700

    Add perspective and update requests to be sent in intervals.

--- detector/main.py
+++ detector/main.py
@@ -1,26 +1,21 @@
 import os
 from video_processor import process_video
-import requests
-import json
+import cv2
 from location_parser import parse_location
+
+VIDEO_PATH = ".\input_videos"
+location_map = parse_location(os.path.join(VIDEO_PATH, "locations.csv"))
 
 VIDEO_EXTENSIONS = [".mp4"]
 VIDEO_PATH = ".\input_videos"
 
-location_map = parse_location(os.path.join(VIDEO_PATH, "locations.csv"))
+# Set intervals (in frames) to send requests
+REQ_INTERVAL = 30
 
 for fileName in os.listdir(VIDEO_PATH):
     if fileName.endswith(tuple(VIDEO_EXTENSIONS)):
 
         # safe distance in pixels set to 50
-        weight = process_video(os.path.join(VIDEO_PATH, fileName), 30)
+        process_video(os.path.join(VIDEO_PATH, fileName), location_map, REQ_INTERVAL)
 
-        # post data
-        data = {
-                'lat': location_map[fileName]['lat'],
-                'lng': location_map[fileName]['lng'],
-                'weight': weight
-                }
-        json.dumps(data)
-        print(data)
-        r = requests.post('http://localhost:8080', json=data)
+
--- detector/video_processor.py
+++ detector/video_processor.py
@@ -2,9 +2,40 @@ import cv2
 from layers import get_layers
 from detector import detect
 from distance_analysis import analyze_dist
+import numpy as np
+import time
+from requests import send_req
 
-def process_video(video_path, safe_dist):
 
+
+clicks = []
+global frame
+
+def mouse_on(event, x, y, *args, **kwargs):
+    global clicks
+    if event == cv2.EVENT_LBUTTONDOWN:
+        if "clicks" not in globals():
+            clicks = []
+        clicks.append((x, y))
+
+        if len(clicks) <= 4:
+            cv2.circle(frame, (x, y), 2, (0, 0, 255), 2)
+        else:
+            cv2.circle(frame, (x, y), 2, (255, 0, 0), 2)
+            
+        if len(clicks) > 1 and len(clicks) <= 4:
+            cv2.line(frame, (x, y), (clicks[len(clicks)-2][0], clicks[len(clicks)-2][1]), (70, 70, 70), 2)
+            if len(clicks) == 4:
+                cv2.line(frame, (x, y), (clicks[0][0], clicks[0][1]), (70, 70, 70), 2)
+        
+
+
+
+
+
+def process_video(video_path, location_map, REQ_INTERVAL):
+    count = 0
+    global clicks
     # import Common Object in Context categories names
     file_coconames = open(".\detector\yolo-coco\coco.names")
     coco_categories = file_coconames.read().split("\n")
@@ -18,7 +49,15 @@ def process_video(video_path, safe_dist):
     # number of violations in social distance observed over all frames
     unsafe_accumulated = 0
     num_frames = 0
-    
+    global frame
+
+    # configure mouse call back
+    cv2.namedWindow("frame")
+    cv2.setMouseCallback("frame", mouse_on)
+
+    # FPS
+    start_time = time.time()
+
     while True:
 
         ret, frame = video_captured.read()
@@ -31,11 +70,46 @@ def process_video(video_path, safe_dist):
        	new_height = int(frame.shape[0] / frame.shape[1] * 700)
         frame = cv2.resize(frame, (700, new_height))   
            
+        # get mouse clicks
+        if not count:
+            while True:
+                cv2.imshow("frame", frame)
+                cv2.waitKey(1)
+                if len(clicks) == 8:
+                    cv2.destroyWindow("frame")
+                    break
+            four_pts = clicks
+
+
+        # Transform perspective
+        (H, W) = frame.shape[:2]
+        src = np.float32(np.array(four_pts[:4]))
+        dst = np.float32([[0, H], [W, H], [W, 0], [0, 0]])
+        prespective_transform = cv2.getPerspectiveTransform(src, dst)
+        birdseye_result = cv2.warpPerspective(frame, prespective_transform, (500, 600)) 
+
+        # Transformed Capture 
+        cv2.imshow('Birdseye', birdseye_result) 
+
+        # Tranform distance to pixels
+        pts = np.float32(np.array([four_pts[4:7]]))
+        trans_pts = cv2.perspectiveTransform(pts, prespective_transform)[0]
+
+        # Use three points to calculate real distance in pixels for 6 feet in x and y axis
+        xdist_in_pixels = np.sqrt((trans_pts[0][0] - trans_pts[1][0]) ** 2 + (trans_pts[0][1] - trans_pts[1][1]) ** 2)
+        ydist_in_pixels = np.sqrt((trans_pts[0][0] - trans_pts[2][0]) ** 2 + (trans_pts[0][1] - trans_pts[2][1]) ** 2)
+        
+        # Display four points of rectangle
+        cv2.polylines(frame, [np.array(four_pts[:4], np.int32)], True, (255, 255, 255), thickness=2)
+        
+
+
+
         # use YOLO to detect persons in frame
         detections = detect(layers, dnn_net, frame)
 
         # detect unsafe distance
-        unsafe = analyze_dist(detections, safe_dist)
+        unsafe = analyze_dist(detections, prespective_transform, xdist_in_pixels, ydist_in_pixels)
 
 
         # loop over the results to render boxes
@@ -43,6 +117,7 @@ def process_video(video_path, safe_dist):
             (startX, startY, endX, endY) = bbox
             (cX, cY) = centroid
             color = (0, 255, 0)
+
 
             if i in unsafe:
                 color = (0, 0, 255)
@@ -62,8 +137,28 @@ def process_video(video_path, safe_dist):
         cv2.putText(frame, text, (10, frame.shape[0] - 25),
             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)    
 
-        cv2.imshow("Frame", frame)
+        if num_frames == 1:
+            start_time = time.time()
+
+        curr_time = time.time()
+        if (curr_time - start_time) != 0:
+            fps = num_frames / (curr_time - start_time)
+            text = "FPS: {}".format(round(fps, 2))
+            cv2.putText(frame, text, (10, 25),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)    
+
+        # cv2.imshow("Frame", frame)
         key = cv2.waitKey(1) & 0xFF
 
+        if count != 0:
+            cv2.imshow('processed', frame)    
+        count = count + 1
+
+        if (num_frames % REQ_INTERVAL == 0):
+            send_req(unsafe_avg, video_path, location_map)
+
         if key == ord("q") or key == 27:
-            return unsafe_avg
+            cv2.destroyWindow("processed")
+            clicks = []
+    clicks = []
+    
